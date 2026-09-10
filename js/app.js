b0bef9b2ae4b30b77bfa07b6ec45796169adf6a0
Autumn <tripleumo@gmail.com>
2026-09-10 09:04:05 +0300

    feat: navbar, section switching

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 function app() {
     return {
-        
+        Screens: Object.freeze({
+            ABOUT: "ABOUT",
+            PROJECTS: "PROJECTS",
+            SUPPORT: "SUPPORT",
+            SOCIALS: "SOCIALS"
+        }),
+
+        currentScreen: null,
+
+        switchScreen(screen) {
+            this.currentScreen = screen
+        },
+
+        isScreen(screen) {
+            return this.currentScreen === screen
+        },
+
+        // init
+        init() {
+            this.currentScreen = this.Screens.ABOUT
+        }
     }
 }
