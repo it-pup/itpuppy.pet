3cf2bc30e0b585281cbce6e3a794b0e9472bb510
Autumn <git@itpuppy.pet>
2026-09-13 15:17:58 +0300

    feat: css nesting, about section

--- js/app.js
+++ js/app.js
@@ -1,14 +1,45 @@
 function app() {
     return {
-        Screens: Object.freeze({
+        // data
+        Screens: {
             ABOUT: "ABOUT",
             PROJECTS: "PROJECTS",
-            SUPPORT: "SUPPORT",
             SOCIALS: "SOCIALS"
-        }),
-
+        },
         currentScreen: null,
 
+        testimonials: [
+            {
+                from: "mona",
+                url: "https://mona.cat/",
+                text: "Awesome Fluffy Puppy"
+            },
+            {
+                from: "juna",
+                url: "https://github.com/j10a1n15",
+                text: "Autumn makes my tail wag"
+            },
+            {
+                from: "'Sappho 594 BC'",
+                text: "Women :drooling_face:"
+            },
+            {
+                from: "pandy",
+                text: "wruff woof arf awruf awooo"
+            },
+            {
+                from: "rainy",
+                url: "https://github.com/rainymei",
+                text: "awrf :3 🥺"
+            },
+            {
+                from: "box",
+                url: "https://github.com/Bloxigus",
+                text: "meow"
+            }
+        ],
+
+        // functions
         switchScreen(screen) {
             this.currentScreen = screen
         },
@@ -20,6 +51,9 @@ function app() {
         // init
         init() {
             this.currentScreen = this.Screens.ABOUT
+
+            // sort all testimonials by name (alphabetically)
+            this.testimonials.sort((a, b) => a.from.localeCompare(b.from))
         }
     }
 }
