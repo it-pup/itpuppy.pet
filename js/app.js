dec10748dcaefadd39d4791751b3ee0cf4a28b2c
Autumn <git@itpuppy.pet>
2026-09-14 17:05:40 +0300

    feat: dynamic mood update timestamp

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ function app() {
 
         currentScreen: null,
         mobileNavShow: false,
+        moodUpdDate: new Date("2026-09-14"),
 
         testimonials: [
             {
@@ -122,6 +123,18 @@ function app() {
 
         isScreen(screen) {
             return this.currentScreen === screen
+        },
+
+        relativeTime(date, current) {
+            const diff = Math.abs(current - date) / 1000;
+            
+            if (diff < 60)       return Math.floor(diff) + 's';
+            if (diff < 3600)     return Math.floor(diff / 60) + 'm';
+            if (diff < 86400)    return Math.floor(diff / 3600) + 'h';
+            if (diff < 2592000)  return Math.floor(diff / 86400) + 'd';
+            if (diff < 31536000) return Math.floor(diff / 2592000) + 'mo';
+            
+            return Math.floor(diff / 31536000) + 'y';
         },
 
         // init
